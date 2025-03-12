efd6d0093d54ecac0e3a482fa56308c7d47e622f
Sarthak Jaiswal <sarthakom2207@gmail.com>
2025-03-12 18:21:27 +0530

    first commit

---
+++ Frontend/src/components/About.jsx
@@ -0,0 +1,18 @@
+function About(){
+    return (
+      <section id="about">
+        <h1 className="lfh1">About Us</h1>
+        <p className="about-desc">
+        Initiated by Rudhr Chauhan and Sarthak Jaiswal of 4 BCA A, this web application is designed to streamline the process of reporting and recovering lost items within the university. Built using the MERN stack (MongoDB, Express.js, React, Node.js), the platform enables students and faculty to efficiently log missing and found belongings, search for items, and securely connect with finders.
+
+Currently under development, the application aims to provide a seamless and organized approach to lost item recovery, enhancing convenience and accessibility across the campus.
+        </p>
+        <footer>
+            <p>Copyright &copy; 2025</p>
+            <p>Designed and Built by <a href="https://www.linkedin.com/in/sarthak-jaiswal22">Sarthak & <a href="https://www.linkedin.com/in/rudhr-chauhan"> Rudhr</a> </a></p>
+        </footer>
+      </section>
+      
+    );
+}
+export default About;
